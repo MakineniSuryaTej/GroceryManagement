765264b3320b68a488ed0e5a3ae785c599c35473
MakineniSuryaTej <64590688+MakineniSuryaTej@users.noreply.github.com>
2021-06-22 14:50:47 +0530

    changes

--- app.py
+++ app.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def home():
     return render_template('grocery-layout.html')
 
+@app.route('/grocery-login')
+def login():
+    return render_template('grocery-login.html')
+
 if __name__=='__main__':
     app.run(debug=True)
